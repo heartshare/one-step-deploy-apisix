dcd0670332e99a34476b0087281255db2107be11
wangyanfeng <yanfengking@163.com>
2020-10-13 03:53:16 +0800

    bugfix

--- one-step-install.sh
+++ one-step-install.sh
@@ -11,14 +11,15 @@ fi
 echo "create namespace"
 kubectl create namespace ${ns}
 
-echo "deploy-etcd"
+echo "deploy-etcd and sleep 60s"
 kubectl apply -f  deploy-etcd-cluster.yml  -n ${ns}
+sleep 60
 
 echo "deploy-apisix-gw"
-sed -i -e "s%{YOUR_NAMESPACE}%apisix%g" apisix-config.yml
+sed -i -e "s%{YOUR_NAMESPACE}%${ns}%g" apisix-config.yml
 kubectl create configmap  apisix-config   --from-file=apisix-config.yml -n ${ns}
 kubectl apply -f deploy-apisix-gw.yml -n ${ns}
-# 如果集群中没有部署prometheus-operator，则不需要执行
+# Comment it if it prometheus-operator is not installed
 kubectl apply -f apisix-service-monitor-for-prometheus.yml -n ${ns}
 
 echo "deploy-apisix-mysql"
